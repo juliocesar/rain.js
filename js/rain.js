6be3f60a14f8af4ff02bea2fd97f13a4a86cfed8
Julio Cesar Ody <julioody@gmail.com>
2010-11-13 13:49:59 +1100

    it works!

--- js/rain.js
+++ js/rain.js
@@ -1,35 +1,52 @@
 (function(window, undefined) {
   if (typeof Raphael == 'undefined') throw "Rain needs Raphaël.js!";
     
-  Rain = function(element) {
+  Rain = function(element, speed, angle) {
     if (!(this instanceof arguments.callee)) {
       return new arguments.callee(arguments);
     };
     var self = this;
     
-    self.init = function(element) {
+    self.init = function(element, speed, angle, intensity) {
       var elt = document.getElementById(element);
       self.stage = {
           element:  elt,
           width:    elt.getBoundingClientRect().width,
           height:   elt.getBoundingClientRect().height
         };
-      self.canvas = Raphael(self.stage.element, self.stage.width, self.stage.height);
-      var drop = self.canvas.path("M100 50L90 90");
-      drop.attr({
-        stroke: '#FFFFFF'
-      });
-      
+      self.canvas = Raphael(self.stage.element);
+      self.offset = (Math.tan(angle * Math.PI / 180) * self.stage.height);
+      runEngine(speed, angle, intensity);
       return self;
     };
     
-    function randomStartingPoint() {
-      return Math.floor(Math.random * self.stage.width);
+    function runEngine(speed, angle, intensity) {
+      setInterval(function() { createDrop(speed, angle); }, 100 / intensity);
     }
-    self.rand = randomStartingPoint;
     
-    return self.init(element);
+    function randomStartingPoint(angle) {
+      return Math.floor(Math.random() * (self.stage.width + self.offset));
+    }
+    
+    function createPositionMatrix(angle) {
+      return [randomStartingPoint(angle), 0, 1, 10];
+    }
+    
+    function createDrop(speed, angle) {
+      var positionMatrix = createPositionMatrix(angle),
+        drop = self.canvas.ellipse.apply(self.canvas, positionMatrix),
+        factor = Math.random(), 
+        layer = speed * (1 + factor),
+        cx = positionMatrix[0] - self.offset;
+      drop
+        .attr({stroke: '#fff', opacity: 1 - factor})
+        .rotate(angle)
+        .animate({cy: self.stage.height, cx: cx}, layer, function() { drop.remove(); });
+      return drop;
+    }
+    
+    return self.init.apply(self, arguments);
   };
   
-  o = new Rain('canvas');
+  o = new Rain('canvas', 500, 20, 1);
 })(window);
